09dc66a1a599dd443d66edc92ada759b883f37b3
Jean-Pierre Matsumoto <jpmat@pc16.home>
2016-03-28 00:02:22 +0200

    Add info for Chef supermarket

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,9 @@ license 'all_rights'
 description 'Installs/Configures JBoss Data Virtualization service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
+source_url 'https://github.com/jpmat296/chef-jboss-dv'
+license 'Apache v2.0'
+supports 'redhat', '~> 5.2'
+supports 'centos', '~> 6.0'
 
 depends 'jboss-eap', '~> 2.1.1'
