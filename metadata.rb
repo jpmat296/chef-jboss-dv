302d291451bd335165aa46e3a9a79edcef7b00d4
Jean-Pierre Matsumoto <jpmat@pc16.home>
2016-03-28 00:10:04 +0200

    Update version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jpmat296@gmail.com'
 license 'all_rights'
 description 'Installs/Configures JBoss Data Virtualization service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 source_url 'https://github.com/jpmat296/chef-jboss-dv'
 license 'Apache v2.0'
 supports 'redhat', '~> 5.2'
